4fdf94a6d2aa9a395bf3f9eeb1512a5b94f8b03e
chenyu <@chenyu>
2019-02-17 16:42:19 +0800

    b change

--- src/main/java/com/baizhi/cmfz/controller/UserController.java
+++ src/main/java/com/baizhi/cmfz/controller/UserController.java
@@ -2,10 +2,6 @@ package com.baizhi.cmfz.controller;
 
 public class UserController {
     public void test() {
-        System.out.println();
-        System.out.println("aaa");
-        System.out.println();
-        System.out.println("哎 呀 我 去");
-        
+
     }
 }
